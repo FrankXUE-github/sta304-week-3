9bcd36a311ee0a7e341f8652a764af02dc429950
FrankXUE-github <feiyang.xue@mail.utoronto.ca>
2024-09-19 20:36:57 -0400

    Add files via upload

---
+++ scripts/05-replications.R
@@ -0,0 +1,20 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: Feiyang Xue
+# Date: 19 September 2024
+# Contact: feiyang.xue@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+library(quarto)
+quarto_render("index.qmd",output_format = "pdf")
